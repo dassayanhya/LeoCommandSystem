50818cdaf9416f62fc469b2053dda5eb0b8ba707
dassayanhya <saynhyadas83@gmail.com>
2025-11-19 18:10:50 +0530

    Dynamic Link & Logic Fixed

--- js/bodycam.js
+++ js/bodycam.js
@@ -154,6 +154,13 @@ const categories = [
       "/me Break the trunk wide open using a crowbar",
       '/me writes down license plate and searches the license plate through the ${org} database',
       '/me Kneels down, feels the VIN number under the car and run it through ${org} Database',
+
+      // ---- NEW FIX: Only show dispatch for allowed orgs ----
+      ...( ["LSPD", "SAHP", "FIB"].includes(org)
+          ? [`${badge} to dispatch : Show me 10-42 at ${londonTime}`]
+          : []
+      )
+        // -------------------------------------------------------
     ],
   },
 
@@ -164,6 +171,13 @@ const categories = [
       "/me grabs a *SIZE* jumpsuit from behind the counter",
       "/me Places the Jumpsuit on the Bed",
       "/try Removes Cell-Phone, Two-way Radio and Lockpicks from the person in front",
+
+      // ---- NEW FIX: Only show dispatch for allowed orgs ----
+      ...( ["LSPD", "SAHP", "FIB"].includes(org)
+          ? [`${badge} to dispatch : Show me 10-42 at ${londonTime}`]
+          : []
+      )
+        // -------------------------------------------------------
     ],
   },
 
